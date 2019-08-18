7874e70c77318a5972e6a2cac5e3215bd7b91efd
Emil Djupvik <emil14833@gmail.com>
2019-08-18 16:52:00 +0200

    adjust theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Find Devs',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
       home: HomeRoute(),
     );
