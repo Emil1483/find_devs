0257158ebe63a38d4e1ac38474e04a075b586700
Emil Djupvik <emil14833@gmail.com>
2019-08-18 20:13:19 +0200

    add auth UI

--- lib/routes/auth_route.dart
+++ lib/routes/auth_route.dart
@@ -3,6 +3,99 @@ import 'package:flutter/material.dart';
 class AuthRoute extends StatelessWidget {
   static const routeName = "/auth";
 
+  Widget _buildLogo() {
+    return Container(
+      alignment: Alignment.center,
+      height: 144.0,
+      margin: EdgeInsets.symmetric(vertical: 16.0),
+      child: Image(
+        image: AssetImage("assets/missing_asset.png"),
+      ),
+    );
+  }
+
+  List<Widget> _buildFormFields() {
+    return <Widget>[
+      TextFormField(
+        validator: (String input) {
+          if (input.isEmpty) return "Please type email";
+          return null;
+        },
+        decoration: InputDecoration(
+          labelText: "Email",
+          icon: Icon(Icons.mail),
+        ),
+      ),
+      TextFormField(
+        validator: (String input) {
+          if (input.isEmpty) return "Please type email";
+          return null;
+        },
+        decoration: InputDecoration(
+          labelText: "Password",
+          icon: Icon(Icons.lock),
+        ),
+        obscureText: true,
+      ),
+    ];
+  }
+
+  Widget _buildLogin() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Text("Login"),
+      onPressed: () {},
+    );
+  }
+
+  Widget _buildGoogleLogin() {
+    return RaisedButton(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      onPressed: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            "assets/google_logo.png",
+            width: 22.0,
+          ),
+          SizedBox(width: 8.0),
+          Text(
+            "Sign in with Google",
+            style: TextStyle(
+              color: Colors.black54,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCreateAcc(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          Feedback.forLongPress(context);
+        },
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 12.0),
+          child: Text(
+            "Create an Account",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +103,24 @@ class AuthRoute extends StatelessWidget {
         title: Text(
           "Login",
           style: Theme.of(context).textTheme.title,
+        ),
+      ),
+      body: Form(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 22.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildLogo(),
+                ..._buildFormFields(),
+                SizedBox(height: 22.0),
+                _buildLogin(),
+                _buildGoogleLogin(),
+                _buildCreateAcc(context),
+              ],
+            ),
+          ),
         ),
       ),
     );
