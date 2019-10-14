1373b6f846b5103e7063f50d6f8495b4a476581f
Emil Djupvik <emil14833@gmail.com>
2019-10-14 22:41:51 +0200

    add send ability

--- lib/routes/chat_route.dart
+++ lib/routes/chat_route.dart
@@ -13,6 +13,14 @@ class ChatRoute extends StatefulWidget {
 }
 
 class _ChatRouteState extends State<ChatRoute> {
+  TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Chat chat = Provider.of<Chat>(context);
@@ -20,28 +28,75 @@ class _ChatRouteState extends State<ChatRoute> {
       appBar: AppBar(
         title: Text(chat.userData.username),
       ),
-      body: StreamBuilder(
-        stream: chat.stream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            final dokuments = snapshot.data.documents;
-            return ListView.builder(
-              itemCount: snapshot.data.documents.length,
-              reverse: true,
-              itemBuilder: (context, index) => Message(
-                messageData: MessageData.fromMap(
-                  dokuments[index].data,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder(
+              stream: chat.stream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  final dokuments = snapshot.data.documents;
+                  return ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    reverse: true,
+                    itemBuilder: (context, index) => Message(
+                      messageData: MessageData.fromMap(
+                        dokuments[index].data,
+                      ),
+                      selfUid: chat.uid,
+                      otherUserData: chat.userData,
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: TextField(
+                      controller: _textController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 6,
+                      minLines: 1,
+                      decoration: InputDecoration(
+                        hintText: "Type a message...",
+                        contentPadding: EdgeInsets.zero,
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
                 ),
-                selfUid: chat.uid,
-                otherUserData: chat.userData,
-              ),
-            );
-          }
-        },
+                SizedBox(width: 4.0),
+                IconButton(
+                  color: Theme.of(context).accentColor,
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    if (_textController.text.isEmpty) return;
+                    chat.sendMessage(_textController.text);
+                    _textController.clear();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/ui_elements/message.dart
+++ lib/ui_elements/message.dart
@@ -15,6 +15,41 @@ class Message extends StatelessWidget {
     @required this.otherUserData,
   });
 
+  String _format(DateTime dateTime) {
+    return "${dateTime.day}. ${_monthFromInt(dateTime.month)} ${dateTime.year.toString().substring(2)}";
+  }
+
+  String _monthFromInt(int index) {
+    assert(index >= 0 && index <= 12);
+    switch (index) {
+      case 1:
+        return "Jan";
+      case 2:
+        return "Feb";
+      case 3:
+        return "Mar";
+      case 4:
+        return "Apr";
+      case 5:
+        return "May";
+      case 6:
+        return "Jun";
+      case 7:
+        return "Jul";
+      case 8:
+        return "Aug";
+      case 9:
+        return "Sept";
+      case 10:
+        return "Oct";
+      case 11:
+        return "Nov";
+      case 12:
+        return "Dec";
+    }
+    return "";
+  }
+
   void _showDialog(BuildContext context) {
     showDialog(
       context: context,
@@ -46,7 +81,6 @@ class Message extends StatelessWidget {
         );
       },
     );
-  
   }
 
   Widget _buildIcon() {
@@ -67,44 +101,95 @@ class Message extends StatelessWidget {
     }
   }
 
+  Widget _buildMain(BuildContext context, {bool sentBySelf}) {
+    return Row(
+      mainAxisAlignment:
+          sentBySelf ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: <Widget>[
+        if (!sentBySelf)
+          GestureDetector(
+            child: _buildIcon(),
+            onTap: () => _showDialog(context),
+          ),
+        Container(
+          constraints: BoxConstraints.loose(
+            Size(
+              MediaQuery.of(context).size.width * 0.7,
+              double.infinity,
+            ),
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18.0),
+            color: sentBySelf
+                ? Theme.of(context).accentColor
+                : Theme.of(context).cardColor,
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 13.0, vertical: 10.0),
+          child: Text(
+            messageData.content,
+            style: TextStyle(
+              color: sentBySelf ? Theme.of(context).canvasColor : Colors.white,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final bool sentBySelf = messageData.from == selfUid;
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-      child: Row(
-        mainAxisAlignment:
-            sentBySelf ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: <Widget>[
-          if (!sentBySelf)
-            GestureDetector(
-              child: _buildIcon(),
-              onTap: () => _showDialog(context),
-            ),
-          Container(
-            constraints: BoxConstraints.loose(
-              Size(
-                MediaQuery.of(context).size.width * 0.7,
-                double.infinity,
-              ),
-            ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18.0),
-              color: sentBySelf
-                  ? Theme.of(context).accentColor
-                  : Theme.of(context).cardColor,
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 13.0, vertical: 10.0),
-            child: Text(
-              messageData.content,
-              style: TextStyle(
-                color:
-                    sentBySelf ? Theme.of(context).canvasColor : Colors.white,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
+
+    List<Widget> children = <Widget>[
+      if (!sentBySelf)
+        GestureDetector(
+          child: _buildIcon(),
+          onTap: () => _showDialog(context),
+        ),
+      Container(
+        constraints: BoxConstraints.loose(
+          Size(
+            MediaQuery.of(context).size.width * 0.7,
+            double.infinity,
           ),
-        ],
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18.0),
+          color: sentBySelf
+              ? Theme.of(context).accentColor
+              : Theme.of(context).cardColor,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 13.0, vertical: 10.0),
+        child: Text(
+          messageData.content,
+          style: TextStyle(
+            color: sentBySelf ? Theme.of(context).canvasColor : Colors.white,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+      SizedBox(width: 12.0),
+      Text(
+        _format(
+          DateTime.fromMillisecondsSinceEpoch(
+            int.parse(messageData.timestamp),
+          ),
+        ),
+        style: TextStyle(fontSize: 12.0),
+      ),
+    ];
+
+    if (sentBySelf) children = children.reversed.toList();
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      reverse: sentBySelf,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+        child: Row(
+          children: children,
+        ),
       ),
     );
   }
