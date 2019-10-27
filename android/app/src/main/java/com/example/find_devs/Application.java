4446d2b421db8eec32a3a4c3e0b91de51ba3feb4
Emil Djupvik <emil14833@gmail.com>
2019-10-27 19:51:47 +0100

    revert prev change

--- android/app/src/main/java/com/example/find_devs/Application.java
+++ android/app/src/main/java/com/example/find_devs/Application.java
@@ -1,4 +1,4 @@
-package tech.djupvik.find_devs;
+package com.example.find_devs;
 
  import io.flutter.app.FlutterApplication;
  import io.flutter.plugin.common.PluginRegistry;
--- android/app/src/main/java/com/example/find_devs/MainActivity.java
+++ android/app/src/main/java/com/example/find_devs/MainActivity.java
@@ -1,4 +1,4 @@
-package tech.djupvik.find_devs;
+package com.example.find_devs;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
