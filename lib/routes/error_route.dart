58d68b8cb31acef63143986a93d4466019f679b7
Emil Djupvik <emil14833@gmail.com>
2019-11-05 22:22:27 +0100

    improve error route

--- lib/routes/error_route.dart
+++ lib/routes/error_route.dart
@@ -11,29 +11,43 @@ class ErrorRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+    bool portrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+    List<Widget> children = <Widget>[
+      Expanded(
+        flex: portrait ? 3 : 1,
+        child: Image.asset("assets/broken.png"),
+      ),
+      Expanded(
+        child: Center(
+          child: GradientButton(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            onPressed: () {
+              Provider.of<Devs>(context).init();
+              Navigator.of(context).pushReplacementNamed(HomeRoute.routeName);
+            },
+            gradient: LinearGradient(
+              colors: [
+                theme.accentColor,
+                theme.indicatorColor,
+              ],
+            ),
+            child: Text(
+              "Try again",
+              style: theme.textTheme.button,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Error"),
       ),
       body: Center(
-        child: GradientButton(
-          padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          onPressed: () {
-            Provider.of<Devs>(context).init();
-            Navigator.of(context).pushReplacementNamed(HomeRoute.routeName);
-          },
-          gradient: LinearGradient(
-            colors: [
-              theme.accentColor,
-              theme.indicatorColor,
-            ],
-          ),
-          child: Text(
-            "Try again",
-            style: theme.textTheme.button,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
+        child: portrait ? Column(children: children) : Row(children: children),
       ),
     );
   }
